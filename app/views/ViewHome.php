d237404516aec07b95faf90625109c70f3eb9c2f
victoria ledesma <vickyledesma988@gmail.com>
2025-01-29 07:55:51 -0300

    add home

---
+++ app/views/ViewHome.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/primerachamba/estilos/estilos.css">
+    <title>Home</title>
+</head>
+<body>
+    <header>
+        <img src="/primerachamba/static/Logo.png" alt="Logo" class="logo">
+    </header>
+    <main class="home-container">
+        <button class="home-button" id="moto">Moto</button>
+        <button class="home-button" id="cliente">Cliente</button>
+        <button class="home-button" id="turno">Turno</button>
+    </main>
+</body>
+</html>
