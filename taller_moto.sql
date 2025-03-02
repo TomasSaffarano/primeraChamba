fb4366691b069fafd51441e2ebd89873de2cfc77
Tomas Saffarano <90287654+TomasSaffarano@users.noreply.github.com>
2025-03-02 18:51:53 -0300

    db

---
+++ taller_moto.sql
@@ -0,0 +1,177 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 02-03-2025 a las 22:50:59
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `taller_moto`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `usuario` varchar(50) NOT NULL,
+  `contraseña` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `admin`
+--
+
+INSERT INTO `admin` (`id`, `usuario`, `contraseña`) VALUES
+(1, 'admin', '$2y$10$iea226b.FqkNnk85OuWMU.vbryuiLfFqq3/qUlONDoCg3kypBz./S');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(60) NOT NULL,
+  `dni` int(11) NOT NULL,
+  `telefono` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `cliente`
+--
+
+INSERT INTO `cliente` (`id`, `nombre`, `dni`, `telefono`) VALUES
+(3, 'vicky sela come', 56756578, 5675),
+(5, 'chichita rodriguez', 456465, 456),
+(17, 'prueba', 56785, 44);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `moto`
+--
+
+CREATE TABLE `moto` (
+  `id` int(11) NOT NULL,
+  `modelo` varchar(30) NOT NULL,
+  `patente` varchar(20) NOT NULL,
+  `estado` varchar(20) NOT NULL,
+  `descripcion` varchar(300) DEFAULT NULL,
+  `observaciones` varchar(300) DEFAULT NULL,
+  `kilometros` int(11) DEFAULT NULL,
+  `dni` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `moto`
+--
+
+INSERT INTO `moto` (`id`, `modelo`, `patente`, `estado`, `descripcion`, `observaciones`, `kilometros`, `dni`) VALUES
+(16, 'Prueba', '5675', 'en_reparacion', '5675', '576675', 56757, 56756578),
+(23, 'Prueba2', '5676', 'terminada', '345635', '34563', 34563, 456465),
+(25, 'Prueba3', '5678', 'terminada', 'ryterty', 'ertyeyt', 45647, 56785),
+(26, 'Prueba4', '7667', 'terminada', '4567476', '45674567', 45674, 56785);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `turno`
+--
+
+CREATE TABLE `turno` (
+  `id` int(11) NOT NULL,
+  `ingreso` date NOT NULL,
+  `entrega` date NOT NULL,
+  `patente` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `dni` (`dni`);
+
+--
+-- Indices de la tabla `moto`
+--
+ALTER TABLE `moto`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `patente` (`patente`),
+  ADD KEY `fk_dni` (`dni`);
+
+--
+-- Indices de la tabla `turno`
+--
+ALTER TABLE `turno`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT de la tabla `moto`
+--
+ALTER TABLE `moto`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+
+--
+-- AUTO_INCREMENT de la tabla `turno`
+--
+ALTER TABLE `turno`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `moto`
+--
+ALTER TABLE `moto`
+  ADD CONSTRAINT `moto_ibfk_1` FOREIGN KEY (`dni`) REFERENCES `cliente` (`dni`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
